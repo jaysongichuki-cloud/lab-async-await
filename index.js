f3f0769f6b05b6307021cac7c67e89dfeff31c78
Jayson <jayson.gichuki@student.moringaschool.com>
2026-04-16 02:55:39 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here!
+function displayPosts(posts) {
+    const postList = document.getElementById("post-list");
+
+    posts.forEach(post => {
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1');
+        const p = document.createElement('p');
+
+        h1.textContent = post.title;
+        p.textContent = post.body;
+
+        li.appendChild(h1);
+        li.appendChild(p);
+        postList.appendChild(li);
+    });
+}
+
+async function fetchPosts() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+
+        displayPosts(data);
+        return data;
+    } catch (error) {
+        console.error("error fetching data", error);
+    }
+}
+
+fetchPosts();
